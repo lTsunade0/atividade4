b9682836b279a8122fe536b484a0ac6843222e5d
lTsunade0 <diegoharno@gmail.com>
2024-06-13 13:27:56 -0400

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <HeaderComponent />
+    <ContentComponent />
+  </div>
+</template>
+
+<script>
+import HeaderComponent from './components/HeaderComponent.vue'
+import ContentComponent from './components/ContentComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HeaderComponent,
+    ContentComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
